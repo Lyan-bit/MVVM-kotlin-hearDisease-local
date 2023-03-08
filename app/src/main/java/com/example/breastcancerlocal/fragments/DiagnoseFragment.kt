78d57f495bf040c9b2a272970c1a8499ab0bf3e6
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-03-08 22:40:59 +0000

    Update DiagnoseFragment.kt

--- app/src/main/java/com/example/breastcancerlocal/fragments/DiagnoseFragment.kt
+++ app/src/main/java/com/example/breastcancerlocal/fragments/DiagnoseFragment.kt
@@ -81,15 +81,15 @@ class DiagnoseFragment : Fragment(), View.OnClickListener, AdapterView.OnItemSel
 
         when (v?.id) {
             R.id.diagnoseOK -> {
-                diagnoseOK(v)
+                diagnoseOK()
             }
             R.id.diagnoseCancel -> {
-                diagnoseCancel(v)
+                diagnoseCancel()
             }
         }
     }
 
-    private fun diagnoseOK (v: View?) {
+    private fun diagnoseOK () {
         diagnosebean.setclassification(diagnoseData)
         if (diagnosebean.isdiagnoseerror()) {
             Log.w(javaClass.name, diagnosebean.errors())
@@ -100,7 +100,7 @@ class DiagnoseFragment : Fragment(), View.OnClickListener, AdapterView.OnItemSel
         }
     }
 
-    private fun diagnoseCancel (v: View?) {
+    private fun diagnoseCancel () {
         diagnosebean.resetData()
         diagnoseResult.text = ""
     }
