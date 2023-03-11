cff8d2faff9ee13f6f663bb523ace231a3e08225
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-03-11 05:05:11 +0000

    Update SectionsPagerAdapter.kt

--- app/src/main/java/com/example/breastcancerlocal/adapter/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/breastcancerlocal/adapter/SectionsPagerAdapter.kt
@@ -5,8 +5,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.example.breastcancerlocal.fragments.DiagnoseFragment
-import com.example.breastcancerlocal.fragments.createFragment
-import com.example.breastcancerlocal.fragments.listFragment
+import com.example.breastcancerlocal.fragments.CreateFragment
+import com.example.breastcancerlocal.fragments.ListFragment
 
 class SectionsPagerAdapter(private val mContext: Context, fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
     companion object {
@@ -16,15 +16,15 @@ class SectionsPagerAdapter(private val mContext: Context, fm: FragmentManager) :
     override fun getItem(position: Int): Fragment { // instantiate a fragment for the page.
         return when (position) {
             0 -> {
-                createFragment.newInstance(mContext)
+                CreateFragment.newInstance(mContext)
             }
             1 -> {
-                listFragment.newInstance(mContext)
+                ListFragment.newInstance(mContext)
             }
             2 -> {
                 DiagnoseFragment.newInstance(mContext)
             }
-            else -> listFragment.newInstance(mContext)
+            else -> ListFragment.newInstance(mContext)
         }
     }
 
